23be58c94e06cd6b0d6c82000293b02dd2e8390a
Zireael07 <zi@mail.com>
2017-03-09 14:33:55 +0100

    add a viewport and camera for game world stuff

--- core/src/com/veins/game/screens/GameScreen.java
+++ core/src/com/veins/game/screens/GameScreen.java
@@ -9,7 +9,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.veins.game.MyVeinsGame;
 import com.veins.game.logic.GameLogic;
 import com.veins.game.logic.objects.Player;
@@ -22,10 +25,19 @@ public class GameScreen extends DefaultScreen implements InputProcessor {
     SpriteBatch batch;
     GameLogic logic;
     Player player;
+    OrthographicCamera camera;
+    ScalingViewport viewport;
+    
     
     public GameScreen(MyVeinsGame _game) {
         super(_game);
         logic = new GameLogic();
+        
+        //camera stuffs
+        camera = new OrthographicCamera(logic.MAP_WIDTH*logic.TILE_WIDTH, logic.MAP_HEIGHT*logic.TILE_HEIGHT);
+        camera.translate(camera.viewportWidth/2,camera.viewportHeight/2);
+        //let's have some padding
+        viewport = new ScalingViewport(Scaling.none, logic.MAP_WIDTH*logic.TILE_WIDTH+5, logic.MAP_HEIGHT*logic.TILE_HEIGHT+5, camera);
         
         batch = new SpriteBatch();
         //add player
@@ -41,7 +53,11 @@ public class GameScreen extends DefaultScreen implements InputProcessor {
      public void render (float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-         
+        
+        //camera
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        
         batch.begin();
         //know our fps
         Gdx.graphics.setTitle(""+Gdx.graphics.getFramesPerSecond());
@@ -112,4 +128,9 @@ public class GameScreen extends DefaultScreen implements InputProcessor {
         return false;
     }
     
+    @Override
+    public void resize(int width, int height)
+    {
+        viewport.update(width, height);
+    }
 }
