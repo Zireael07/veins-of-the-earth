793d84350028bd3e7a2f692d5ab2370623b15562
Zireael07 <zi@mail.com>
2017-03-13 12:16:51 +0100

    initialize a rng from SquidLib; function to spawn an actor

--- core/src/com/veins/game/logic/GameLogic.java
+++ core/src/com/veins/game/logic/GameLogic.java
@@ -6,9 +6,12 @@
 package com.veins.game.logic;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.veins.game.logic.objects.Actor;
 import com.veins.game.logic.objects.Player;
+import squidpony.squidmath.StatefulRNG;
 
 /**
  *
@@ -25,20 +28,41 @@ public class GameLogic {
     public static final int TILE_WIDTH = 32;
     public static final int TILE_HEIGHT = 32;
     
+    public StatefulRNG rng;
+    
+    public static int NUM_NPC = 4;
+    
     Player player;
        
     public GameLogic()
     {
+        rng = new StatefulRNG();
+        
+        
         player = new Player(
         MathUtils.random(MAP_WIDTH/2),
         MathUtils.random(MAP_HEIGHT/2),
         this
         );
+    
     }
     
     public Player getPlayer()
     {
         return player;
+    }
+    
+    public Actor spawnActor(Sprite tile, int x, int y){
+        Actor actor = new Actor(x, y,
+            //MathUtils.random(MAP_WIDTH-1),
+            //MathUtils.random(MAP_HEIGHT-1),
+        this);
+        
+        actor.set(tile);
+        actor.Place();
+        Gdx.app.log("Actor", "Spawned an actor at " + x + " " + y);
+        
+        return actor;
     }
     
     public float getYOffset()
@@ -75,13 +99,13 @@ public class GameLogic {
          //vector from bottom to origin
          Vector3 bottom = new Vector3 (ISO_WIDTH/2, origin.y-ISO_HEIGHT/2, 0);
          Vector3 right = new Vector3(bottom.x-origin.x, bottom.y-origin.y, 0);
-         Gdx.app.log("IsotoWorld", "right vec is" + right);
+         //Gdx.app.log("IsotoWorld", "right vec is" + right);
          
          //vector from top to origin
          Vector3 top = new Vector3(ISO_WIDTH/2, origin.y+ISO_HEIGHT/2, 0);
         Vector3 up = new Vector3(top.x-origin.x, top.y-origin.y, 0); 
          //Gdx.app.log("IsotoWorld", "top vec is" + top);
-         Gdx.app.log("IsotoWorld", "up vec is" + up);
+         //Gdx.app.log("IsotoWorld", "up vec is" + up);
          
          //add
          Vector3 target_right = new Vector3(right.x*iso.x, right.y*iso.x, 0);
